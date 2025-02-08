2069bee102b26fe19412c2133690ca9d7b26ca6a
Tim Lin <lintim03@gmail.com>
2025-02-08 17:15:58 -0600

    Create Function for Submit

--- src/CheckList.jsx
+++ src/CheckList.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react"
+
 function CheckList() {
+    const [fontSize, setFontSize] = useState(50)
+    const [checkList, setCheckList] = useState([])
+    const [doneList, setDoneList] = useState([])
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        checkList.push(1)
+        setCheckList(checkList.slice())
+        console.log("wow", checkList.length)
+    }
 
     return (
-        <div>
-            test
+        <div style={{fontSize: fontSize}}>
+            <div className="input">
+                <form onSubmit={handleSubmit}>
+                    <input type="text"/>
+                    <input type="submit"/>
+                </form>
+            </div>
+
+            {checkList.length != 0 && 
+            checkList.map((item) => (
+                <div>wow</div>
+            ))}
+
         </div>
     )
 }
