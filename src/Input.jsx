c2f329f857914e3353159a56a0480476d99573c6
Tim Lin <lintim03@gmail.com>
2025-02-08 21:12:15 -0600

    Require and reset input

--- src/Input.jsx
+++ src/Input.jsx
@@ -3,6 +3,10 @@ import { useState } from "react"
 function Input({handleSubmit, fontSize}){
     const [text, setText] = useState("")
 
+    const resetText = () => {
+        setText("")
+    }
+
     const handleOnChange = (e) => {
         setText(e.target.value)
     }
@@ -11,8 +15,8 @@ function Input({handleSubmit, fontSize}){
         <div className="input" >
             <span style={{fontSize: fontSize}}>Enter a comma seperated list or individual items to add them to your checklist!</span>
 
-            <form onSubmit={(e) => handleSubmit(e, text)}>
-                <input type="text" value={text} onChange={handleOnChange} style={{fontSize: fontSize}}/>
+            <form onSubmit={(e) => {handleSubmit(e, text); resetText();}}>
+                <input type="text" value={text} onChange={handleOnChange} style={{fontSize: fontSize}} required/>
                 <input type="submit" value={"Add Item"} style={{fontSize: fontSize}}/>
             </form>
         </div>
