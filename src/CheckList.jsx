e3896fbe9eeeba5957b1f26f2f2d7d243e181b3c
Tim Lin <lintim03@gmail.com>
2025-02-08 21:57:45 -0600

    Move items back and forth onClick

--- src/CheckList.jsx
+++ src/CheckList.jsx
@@ -7,20 +7,35 @@ function CheckList() {
 
     const [checkList, setCheckList] = useState([])
     const [doneList, setDoneList] = useState([])
-    
 
     const handleSubmit = (e, text) => {
         e.preventDefault()
         checkList.push(text)
         setCheckList(checkList.slice())
     }
+    
+    const moveItem = (item, items) => {
+        items.splice(items.indexOf(item), 1)
+        console.log(item, items)
+        if (items == checkList){
+            doneList.push(item)
+            console.log("check")
+        }
+        else{
+            checkList.push(item)
+            console.log("done")
+        }
+        setCheckList(checkList.slice())
+        setDoneList(doneList.slice())
+        console.log(doneList, checkList)
+    }
 
     return (
         <div>
             <Input handleSubmit={handleSubmit} fontSize={fontSize}/>
 
-            {checkList.length != 0 && <Items items={checkList} startID={0} fontSize={fontSize}/>}
-
+            {checkList.length != 0 && <Items items={checkList} moveItem={moveItem} startID={0} label={"CheckList"} fontSize={fontSize}/>}
+            {doneList.length != 0 && <Items items={doneList} moveItem={moveItem} startID={checkList.length} label={"Completed"} fontSize={fontSize}/>}
         </div>
     )
 }
--- src/Items.jsx
+++ src/Items.jsx
@@ -1,18 +1,21 @@
-function Items({items, startID, fontSize}){
-    console.log(items)
+function Items({items, startID, fontSize, moveItem, label}){
     var count = startID
     const list = items.map(function(item, count){
         return (
-            <div className="item" key={count}>
-                    {item} {count}
+            <div className="item" key={count} onClick={() => moveItem(item, items)}>
+                    {item}
             </div>
         )
     })
 
     return (
-        <div className="items" style={{fontSize: fontSize}}>
-            {list}
+        <div className="list-container">
+            <span className="label" style={{fontSize: fontSize+2}}>{label}</span>
+            <div className="items" style={{fontSize: fontSize}}>
+                {list}
+            </div>
         </div>
+        
     )
 }
 
